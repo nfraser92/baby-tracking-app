2aff3b5b63820986f34908a654b14396a494fa90
Niall Fraser <niallfraser92@gmail.com>
2019-06-12 10:35:02 -0500

    uploading a book works with image

--- Capstone/Controllers/BooksController.cs
+++ Capstone/Controllers/BooksController.cs
@@ -7,17 +7,25 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Capstone.Data;
 using Capstone.Models;
+using Capstone.Models.ViewModels.Books;
+using Microsoft.AspNetCore.Identity;
+using System.IO;
 
 namespace Capstone.Controllers
 {
     public class BooksController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public BooksController(ApplicationDbContext context)
+
+        public BooksController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+        private Task<ApplicationUser> GetCurrentUserAsync() =>
+            _userManager.GetUserAsync(HttpContext.User);
 
         // GET: Books
         public async Task<IActionResult> Index()
@@ -49,9 +57,11 @@ namespace Capstone.Controllers
         // GET: Books/Create
         public IActionResult Create()
         {
+            BookUploadPictureViewModel model = new BookUploadPictureViewModel();
+            model.Book = new Book();
             ViewData["BookTypeId"] = new SelectList(_context.BookType, "BookTypeId", "Description");
             ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
-            return View();
+            return View(model);
         }
 
         // POST: Books/Create
@@ -59,17 +69,40 @@ namespace Capstone.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("BookId,BookTypeId,Title,UserId,ImagePath,Author,Quantity,IsOutgrown")] Book book)
+        public async Task<IActionResult> Create(BookUploadPictureViewModel model)
         {
+            // Remove user from model validation as it is not submitted in the form.
+            // Validation will fail is this is not in here
+            ModelState.Remove("UserId");
+
+            
+            var user = await GetCurrentUserAsync();
+
             if (ModelState.IsValid)
             {
-                _context.Add(book);
+                model.Book.User = user;
+                model.Book.UserId = user.Id;
+
+                if (model.ImageFile != null)
+                {
+                    var fileName = Path.GetFileName(model.ImageFile.FileName);
+                    Path.GetTempFileName();
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", fileName);
+                    // var filePath = Path.GetTempFileName();
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await model.ImageFile.CopyToAsync(stream);
+                        // validate file, then move to CDN or public folder
+                    }
+                    model.Book.ImagePath = model.ImageFile.FileName;
+                }
+                _context.Add(model.Book);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["BookTypeId"] = new SelectList(_context.BookType, "BookTypeId", "Description", book.BookTypeId);
-            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", book.UserId);
-            return View(book);
+            ViewData["BookTypeId"] = new SelectList(_context.BookType, "BookTypeId", "Description", model.Book.BookTypeId);
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", model.Book.UserId);
+            return View(model);
         }
 
         // GET: Books/Edit/5
---
+++ Capstone/Migrations/20190612143113_addingBookType.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Capstone.Migrations
+{
+    public partial class addingBookType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "BookType",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "4f555f8c-d5db-43b5-836c-ffffffffffff",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "186eb298-8621-4b6e-9717-eea8dd977fb4", "AQAAAAEAACcQAAAAEDx0KDAcF7VQ6NkXLuuxc28IYMCXQOPbzA1hq9gh88D+flic0z0uVa+g31qhQW1VlQ==" });
+
+            migrationBuilder.InsertData(
+                table: "BookType",
+                columns: new[] { "BookTypeId", "Description", "UserId" },
+                values: new object[] { 1, "Musical/Sounds", "4f555f8c-d5db-43b5-836c-ffffffffffff" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookType_UserId",
+                table: "BookType",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookType_AspNetUsers_UserId",
+                table: "BookType",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookType_AspNetUsers_UserId",
+                table: "BookType");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookType_UserId",
+                table: "BookType");
+
+            migrationBuilder.DeleteData(
+                table: "BookType",
+                keyColumn: "BookTypeId",
+                keyValue: 1);
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "BookType");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "4f555f8c-d5db-43b5-836c-ffffffffffff",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "be306373-288e-4bd1-b2be-59c7d96c131e", "AQAAAAEAACcQAAAAEJWZZgW7VKIuAAmWESjDKF92MIBQxI8cZXDdbKDQtYMC3erY6N1H0v+FnN8XFywquw==" });
+        }
+    }
+}
