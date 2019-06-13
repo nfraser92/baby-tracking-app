eb15368ddcc943b58c22a2fcf5c062b9c38ff664
Niall Fraser <niallfraser92@gmail.com>
2019-06-13 11:35:54 -0500

    need to get sort direction working

--- Capstone/Controllers/BooksController.cs
+++ Capstone/Controllers/BooksController.cs
@@ -28,8 +28,35 @@ namespace Capstone.Controllers
             _userManager.GetUserAsync(HttpContext.User);
 
         // GET: Books
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string _sortDirection, string _orderBy)
         {
+            string currentSort = "";
+
+            if (_sortDirection == null && _orderBy != null)
+            {
+                ViewData["sortDirection"] = "desc";
+                currentSort = "asc";
+
+            }
+            else if (_sortDirection == null && _orderBy == null)
+            {
+                _orderBy = "Title";
+                currentSort = "asc";
+                ViewData["sortDirection"] = "";
+
+            }
+            else if (_sortDirection == "asc")
+            {
+                ViewData["sortDirection"] = "desc";
+                currentSort = "asc";
+
+            }
+            else if (_sortDirection == "desc")
+            {
+                ViewData["sortDirection"] = "asc";
+                currentSort = "desc";
+            }
+
             var applicationDbContext = _context.Book.Include(b => b.BookType).Include(b => b.User);
             return View(await applicationDbContext.ToListAsync());
         }
